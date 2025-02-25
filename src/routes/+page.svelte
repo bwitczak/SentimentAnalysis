fc51beb19b9e48bc67bee2db9b46977c714331a3
bwitczak <blaise.witczak@gmail.com>
2025-02-25 19:57:24 +0100

    Send form action with form data console log

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
+	import Button from '$lib/components/Button.svelte';
 	import TextArea from '$lib/components/TextArea.svelte';
 
 	let text = $state('');
 </script>
 
-<TextArea bind:text />
+<main class="container">
+	<form method="POST">
+		<TextArea name="sentiment-text" bind:text />
+		<Button type="submit" label="Analize" />
+	</form>
+</main>

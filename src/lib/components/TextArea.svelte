c6ca58c0c18c67bd7d448b2ebb3494f8cca471b3
bwitczak <blaise.witczak@gmail.com>
2025-02-26 21:40:23 +0100

    Add textarea char counter and limit to 500 chars

--- src/lib/components/TextArea.svelte
+++ src/lib/components/TextArea.svelte
@@ -1,22 +1,39 @@
 <script lang="ts">
-	let { text = $bindable(), name }: { text: string; name: string } = $props();
+	let { text = $bindable(''), name }: { text: string; name: string } = $props();
 </script>
 
-<textarea {name} bind:value={text}></textarea>
+<div>
+	<textarea {name} maxlength="500" bind:value={text}></textarea>
+	<span>{text.length}/500</span>
+</div>
 
 <style lang="scss">
-	textarea {
-		padding: $padding-small;
-		resize: none;
-		min-height: 300px;
-		background-color: $modal-bg;
-		border: 1px solid $border-color;
-		border-radius: $border-radius;
+	div {
+		position: relative;
+		box-sizing: border-box;
 
-		&:focus {
-			outline: none;
-			border-color: $primary-color;
-			box-shadow: $focus-shadow;
+		textarea {
+			padding: $padding-small;
+			resize: none;
+			min-height: 400px;
+			background-color: $modal-bg;
+			border: 1px solid $border-color;
+			border-radius: $border-radius;
+			width: 100%;
+			box-sizing: border-box;
+
+			&:focus {
+				outline: none;
+				border-color: $primary-color;
+				box-shadow: $focus-shadow;
+			}
+		}
+
+		span {
+			position: absolute;
+			bottom: $padding-small;
+			right: $padding-small;
+			border-radius: $border-radius;
 		}
 	}
 </style>
